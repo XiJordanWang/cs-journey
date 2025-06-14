b6b8a9630b50ae20cb4c53a17ae73f3d4e5082c9
wangxi <wangxi009567@sina.com>
2025-06-14 14:24:14 +0800

    Finished homework 2

---
+++ cs61a/hw02.py
@@ -0,0 +1,102 @@
+# Q1: Product
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+
+    n: a positive integer
+    term:  a function that takes one argument to produce the term
+
+    >>> product(3, identity)  # 1 * 2 * 3
+    6
+    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)    # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
+    24
+    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
+    162
+    """
+    result = 1
+    while n > 0:
+        result *= term(n)
+        n = n - 1
+    return result
+
+
+# Q2: Accumulate
+def accumulate(fuse, start, n, term):
+    """Return the result of fusing together the first n terms in a sequence
+    and start.  The terms to be fused are term(1), term(2), ..., term(n).
+    The function fuse is a two-argument commutative & associative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11 (fuse is never used)
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    """
+    result = start
+
+    while n > 0:
+        result = fuse(result, term(n))
+        n = n - 1
+    return result
+
+
+# Q3: Make Repeater
+def make_repeater(f, n):
+    """Returns the function that computes the nth application of f.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * (3 * (3 * (3 * (3 * 1))))
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 3)(5) # square(square(square(5)))
+    390625
+    """
+
+    def repeater(x):
+        count = n
+        while count > 0:
+            x = f(x)
+            count = count - 1
+        return x
+
+    return repeater
+
+
+def identity(n):
+    return n
+
+
+def square(n):
+    return n**2
+
+
+def increment(n):
+    return n + 1
+
+
+def triple(n):
+    return n * 3
+
+
+def add(k, n):
+    return k + n
+
+
+def mul(k, n):
+    return k * n
