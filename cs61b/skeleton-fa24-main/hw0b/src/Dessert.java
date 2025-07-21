46ef3173e3d4813e5b79f96a27e0c14e9213d588
wangxi <wangxi009567@sina.com>
2025-07-21 19:02:55 +0800

    Finish Desert

--- cs61b/skeleton-fa24-main/hw0b/src/Dessert.java
+++ cs61b/skeleton-fa24-main/hw0b/src/Dessert.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Dessert {
+
+    public Dessert(int flavor, int price) {
+        this.flavor = flavor;
+        this.price = price;
+        numDeserts++;
+    }
+
+    int flavor;
+    int price;
+
+    static int numDeserts;
+
+    public void printDessert() {
+        System.out.println(flavor + " " + price + " " + numDeserts);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }
 }
