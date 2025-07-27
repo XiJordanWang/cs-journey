2c5d4d44faa6d43db4f0eb9fcb78b5172a10768f
wangxi <wangxi009567@sina.com>
2025-07-27 15:27:51 +0800

    Finish project 1b

--- cs61b/skeleton-fa24-main/proj1b/src/ArrayDeque61B.java
+++ cs61b/skeleton-fa24-main/proj1b/src/ArrayDeque61B.java
@@ -1,48 +1,127 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayDeque61B<T> implements Deque61B<T> {
+
+    private T[] items;
+    private int size;
+
+    private int firstIndex;
+    private int lastIndex;
+
+    public ArrayDeque61B() {
+        items = (T[]) new Object[8];
+        size = 0;
+        firstIndex = 0;
+        lastIndex = 1;
+    }
+
+    private int plusOne(int index) {
+        return (index + 1) % items.length;
+    }
+
+    private int minusOne(int index) {
+        return (index - 1 + items.length) % items.length;
+    }
+
+    private void resize(int capacity) {
+        T[] newItems = (T[]) new Object[capacity];
+        int curr = plusOne(firstIndex);
+        for (int i = 0; i < size; i++) {
+            newItems[i + 1] = items[curr];
+            curr = plusOne(curr);
+        }
+        items = newItems;
+        firstIndex = 0;
+        lastIndex = size + 1;
+    }
+
+    private void checkDownsize() {
+        if (items.length > 16 && size < items.length / 4) {
+            resize(items.length / 2);
+        }
+    }
+
     @Override
     public void addFirst(T x) {
-
+        if (size == items.length - 1) {
+            resize(size * 2);
+        }
+        items[firstIndex] = x;
+        firstIndex = minusOne(firstIndex);
+        size++;
     }
 
     @Override
     public void addLast(T x) {
-
+        if (size == items.length - 1) {
+            resize(items.length * 2);
+        }
+        items[lastIndex] = x;
+        lastIndex = plusOne(lastIndex);
+        size++;
     }
 
     @Override
     public List<T> toList() {
-        return List.of();
+        List<T> result = new ArrayList<>();
+        int curr = plusOne(firstIndex);
+        for (int i = 0; i < size; i++) {
+            result.add(items[curr]);
+            curr = plusOne(curr);
+        }
+        return result;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public T removeFirst() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        firstIndex = plusOne(firstIndex);
+        T firstItem = items[firstIndex];
+        items[firstIndex] = null;
+        size--;
+        checkDownsize();
+        return firstItem;
     }
 
     @Override
     public T removeLast() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        if (items.length > 16 && size < items.length / 4) {
+            resize(items.length / 2);
+        }
+        lastIndex = minusOne(lastIndex);
+        T lastItem = items[lastIndex];
+        items[lastIndex] = null;
+        size--;
+        checkDownsize();
+        return lastItem;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 && index > size()) {
+            return null;
+        }
+        return toList().get(index);
     }
 
     @Override
     public T getRecursive(int index) {
-        return null;
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
     }
 }
--- cs61b/skeleton-fa24-main/proj1b/tests/ArrayDeque61BTest.java
+++ cs61b/skeleton-fa24-main/proj1b/tests/ArrayDeque61BTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -20,4 +21,116 @@ public class ArrayDeque61BTest {
         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
     }
 
+    @Test
+    @DisplayName("Test add first and last")
+    void addFirstAndLast() {
+        ArrayDeque61B<String> l = new ArrayDeque61B<>();
+        assertThat(l.isEmpty()).isEqualTo(true);
+
+        l.addFirst("b");
+        l.addFirst("a");
+        assertThat(l.toList()).containsExactly("a", "b").inOrder();
+
+        l.addLast("c");
+        l.addLast("d");
+        assertThat(l.toList()).containsExactly("a", "b", "c", "d").inOrder();
+        assertThat(l.size()).isEqualTo(4);
+
+        l.addLast("e");
+        l.addLast("f");
+        l.addLast("g");
+        l.addLast("h");
+        l.addLast("i");
+        assertThat(l.toList()).containsExactly("a", "b", "c", "d", "e", "f", "g", "h", "i").inOrder();
+    }
+
+    @Test
+    /** In this test, we have three different assert statements that verify that addFirst works correctly. */
+    public void addFirstTestBasic() {
+        Deque61B<String> lld1 = new ArrayDeque61B<>();
+
+        lld1.addFirst("back"); // after this call we expect: ["back"]}
+        assertThat(lld1.toList()).containsExactly("back").inOrder();
+
+        lld1.addFirst("middle"); // after this call we expect: ["middle", "back"]
+        assertThat(lld1.toList()).containsExactly("middle", "back").inOrder();
+
+        lld1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
+        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+
+         /* Note: The first two assertThat statements aren't really necessary. For example, it's hard
+            to imagine a bug in your code that would lead to ["front"] and ["front", "middle"] failing,
+            but not ["front", "middle", "back"].
+          */
+    }
+
+    @Test
+    /** In this test, we use only one assertThat statement. IMO this test is just as good as addFirstTestBasic.
+     *  In other words, the tedious work of adding the extra assertThat statements isn't worth it. */
+    public void addLastTestBasic() {
+        Deque61B<String> lld1 = new ArrayDeque61B<>();
+
+        lld1.addLast("front"); // after this call we expect: ["front"]
+        lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
+        lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
+        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+
+    @Test
+    /** This test performs interspersed addFirst and addLast calls. */
+    public void addFirstAndAddLastTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+
+         /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+        lld1.addLast(0);   // [0]
+        lld1.addLast(1);   // [0, 1]
+        lld1.addFirst(-1); // [-1, 0, 1]
+        lld1.addLast(2);   // [-1, 0, 1, 2]
+        lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+
+        assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
+    }
+
+    // Below, you'll write your own tests for LinkedListDeque61B.
+
+    @Test
+    public void removeFirstAndRemoveLastTest() {
+        Deque61B<String> lld1 = new ArrayDeque61B<>();
+        lld1.addFirst("Lily");
+        lld1.addFirst("Lucy");
+        lld1.addFirst("Li Lei");
+        lld1.addFirst("Han Meimei");
+
+        String get0 = lld1.get(0);
+        assertThat(get0).isEqualTo("Han Meimei");
+        String get1 = lld1.get(1);
+        assertThat(get1).isEqualTo("Li Lei");
+        String get2 = lld1.get(2);
+        assertThat(get2).isEqualTo("Lucy");
+        String get3 = lld1.get(3);
+        assertThat(get3).isEqualTo("Lily");
+
+        String getR1 = lld1.get(1);
+        assertThat(getR1).isEqualTo("Li Lei");
+        String getR2 = lld1.get(2);
+        assertThat(getR2).isEqualTo("Lucy");
+        String getR3 = lld1.get(3);
+        assertThat(getR3).isEqualTo("Lily");
+
+        int size = lld1.size();
+        assertThat(size).isEqualTo(4);
+
+        String first = lld1.removeFirst();
+        int size1 = lld1.size();
+        assertThat(first).isEqualTo("Han Meimei");
+        assertThat(size1).isEqualTo(3);
+        assertThat(lld1.toList()).containsExactly("Li Lei", "Lucy", "Lily").inOrder();
+
+        String last = lld1.removeLast();
+        int size2 = lld1.size();
+        assertThat(last).isEqualTo("Lily");
+        assertThat(size2).isEqualTo(2);
+        assertThat(lld1.toList()).containsExactly("Li Lei", "Lucy").inOrder();
+    }
 }
