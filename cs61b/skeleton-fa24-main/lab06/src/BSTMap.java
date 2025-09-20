5b984335f4867e22623a2c2ade38fd3d335dacd1
wangxi <wangxi009567@sina.com>
2025-09-20 19:44:29 +0800

    initialize lab06 and lab 07

---
+++ cs61b/skeleton-fa24-main/lab06/src/BSTMap.java
@@ -0,0 +1,46 @@
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K, V> implements Map61B<K, V> {
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return Set.of();
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+
+
+}
